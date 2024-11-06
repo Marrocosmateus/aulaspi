6bbd177ba28d1c1adffb8be06cd3e06f94d0950b
Pedro Mateus Marrocos da Silva <20211204010054@ifrn.local>
2024-11-06 16:45:37 -0300

    Enviando dados do formulario para aplicação

--- src/main/java/ifrn/pi/eventos/controllers/indexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/indexController.java
@@ -1,14 +1,17 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class indexController {
 
-	@RequestMapping ("/")
+	@RequestMapping ("/eventos/for,")
 	public String index() {
 		System.out.println("Chammou o metodo index");
 		return "home";
 	}
+	
+	
 }
